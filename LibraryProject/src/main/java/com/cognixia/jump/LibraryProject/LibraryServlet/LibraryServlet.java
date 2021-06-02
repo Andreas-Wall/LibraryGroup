b8581941dc953a8e27252afeb9cce5d64fb672bf
Paul Cho <47371078+pauldev1128@users.noreply.github.com>
2021-06-02 11:55:40 -0700

    finished the Init,doGet, doPost, Destroy

--- LibraryProject/src/main/java/com/cognixia/jump/LibraryProject/LibraryServlet/LibraryServlet.java
+++ LibraryProject/src/main/java/com/cognixia/jump/LibraryProject/LibraryServlet/LibraryServlet.java
@@ -1,46 +1,68 @@
 package com.cognixia.jump.LibraryProject.LibraryServlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LibraryServlet
- */
+import com.cognixia.jump.LibraryProject.LibraryDAO.LibraryDAO;
+import com.cognixia.jump.LibraryProject.connection.ConnectionManager;
+
+
 public class LibraryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private LibraryDAO libraryDAO;
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		libraryDAO = new LibraryDAO();
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String action  = request.getServletPath();
+		
+		switch(action) {
+		case "/list":
+			listBooks(request, response);
+			break;
+		case "/insert":
+			addNewBooks(request, response);
+			break;
+		case "/edit":
+			editBookInfo(request, response);
+			break;
+		case "/delete":
+			deleteBook(request, response);
+			break;
+		default:
+			response.sendRedirect("index.jsp");
+			break;
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		try {
+			ConnectionManager.getConnection().close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 
 }
